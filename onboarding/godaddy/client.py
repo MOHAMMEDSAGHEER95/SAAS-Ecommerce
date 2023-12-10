0663a5eb42be3bd7719f2b9fab647df8cc971435
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2023-12-10 21:00:16 +0000

    added godaddy client

---
+++ onboarding/godaddy/client.py
@@ -0,0 +1,42 @@
+import json
+import logging
+
+import requests
+from django.conf import settings
+logger = logging.getLogger(__name__)
+
+
+class GoDaddyClientHelper(object):
+    BASE_URL = 'https://api.godaddy.com/v1'
+    API_KEY = settings.GO_DADDY_API_KEY
+    API_SECRET = settings.GO_DADDY_API_SECRET
+    HEADER = "sso-key {}:{}"
+    DOMAIN = settings.PUBLIC_DOMAIN_URL
+
+    def __init__(self):
+        self.HEADER = self.HEADER.format(self.API_KEY, self.API_SECRET)
+
+    def make_api_request(self, url, data=None, method="GET", headers={}):
+        print(url)
+        if method == "GET":
+            logger.info("Request to GoDaddy: {}".format(url))
+            response = requests.get(url, headers=headers)
+        if method == "PATCH":
+            response = requests.patch(url, data=data, headers=self.HEADER)
+        else:
+            logger.info("Request to GoDaddy: {}".format(json.dumps(data)))
+            response = requests.post(url, data=data, headers=headers)
+        logger.info("Response from GoDaddy: {}".format(json.dumps(response.json())))
+        return response
+
+    def add_cname_to_dns(self, name):
+        payload = json.dumps([
+            {
+                "data": "@",
+                "name": name,
+                "ttl": 3600,
+                "type": "CNAME"
+            }
+        ])
+        url = f"{self.BASE_URL}/domains/{self.DOMAIN}/records"
+        response = self.make_api_request(url, payload, method='PATCH')
