6f095529b9590b656b77acd1f41ba680f1c972ff
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2023-12-17 23:05:45 +0000

    added basket checkout success view

--- basket/urls.py
+++ basket/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from basket.views import AddToBasket, BasketDetailView
+from basket.views import AddToBasket, BasketDetailView, stripe_basket_checkout, SuccessView
 
 app_name = 'basket'
 
 urlpatterns = [
     path('add-to-basket', AddToBasket.as_view(), name="add_to_basket"),
     path('view-basket', BasketDetailView.as_view(), name="view_basket"),
+    path('checkout-basket', stripe_basket_checkout, name="checkout_basket"),
+    path('payment-success/<slug:slug>/', SuccessView.as_view(), name="payment_success"),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+import stripe
+from django.conf import settings
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
+from tenant_schemas.utils import schema_context
 
 from basket.models import Basket
+from onboarding.models import Onboarding
 
 
 # Create your views here.
@@ -17,9 +21,40 @@ class AddToBasket(View):
         lines = basket.create_basket_lines(data.get('id'), data.get('quantity'))
         return JsonResponse({"message": "Added to Cart", "count": lines})
 
+
 class BasketDetailView(DetailView):
     model = Basket
     template_name = 'basket/basket.html'
 
     def get_object(self, queryset=None):
         return Basket.objects.get(id=self.request.basket)
+
+
+def stripe_basket_checkout(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    line_items = []
+    basket = Basket.objects.get(id=request.basket)
+    for line in  basket.lines.all():
+        price_data = {"product_data": {"name": line.product.title}, "currency": "gbp", "unit_amount": line.product.price * 100}
+        line_object = {"price_data": price_data, "quantity": line.quantity}
+        line_items.append(line_object)
+    stripe_account = ''
+    schema_name = request.tenant.schema_name
+    with schema_context('public'):
+        stripe_account = Onboarding.objects.get(schema_name=schema_name).stripe_connect_id
+    success_url = f"http://{request.tenant.domain_url}/basket/payment-success/"
+    session = stripe.checkout.Session.create(line_items=line_items,mode="payment",
+                                             success_url=success_url + "{CHECKOUT_SESSION_ID}/",
+                                             cancel_url="http://localhost:8000/cancel"
+                                             )
+    basket.status = basket.SUBMITTED
+    basket.save()
+    del request.session['basket']
+    return HttpResponseRedirect(session.url)
+
+
+class SuccessView(TemplateView):
+    template_name = 'basket/success.html'
+
+
+
