f9a8d1f0e33ded4250f0c6cad352297e58eca32b
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2024-01-10 14:15:34 +0000

    basket merge functionality added

--- basket/middleware.py
+++ basket/middleware.py
@@ -21,21 +21,31 @@ class CustomBasketMiddleware:
                 try:
                     unsigned_basket_id = signer.unsign_object(request.COOKIES['basket'])
                     basket_id = unsigned_basket_id.get("basket_id")
-                    if Basket.objects.get(id=basket_id).status == Basket.SUBMITTED:
+                    if Basket.objects.get(id=basket_id).status != Basket.OPEN:
                         create_basket = True
-
+                    if request.user.is_authenticated is False and Basket.objects.get(id=basket_id).user:
+                        create_basket = True
+                    if request.user.is_authenticated:
+                        old_basket_id = basket_id
+                        basket_id = Basket.get_basket(request.user).id
+                        Basket.merge_basket(old_basket_id, basket_id)
                     request.basket = basket_id
                 except BadSignature:
                     # Handle the case where the cookie value is tampered or invalid
                     pass
             if 'basket' not in request.COOKIES or create_basket:
-                basket = Basket.create_basket(request.user)
+                if request.user.is_authenticated:
+                    create_basket = False
+                    basket = Basket.get_basket(request.user)
+                else:
+                    basket = Basket.create_basket(request.user)
                 basket_id = basket.id
                 request.basket = basket_id
             response = self.get_response(request)
             if 'basket' not in request.COOKIES or create_basket:
                 hash_value = signer.sign_object({"basket_id": basket_id})
-                response.set_cookie("basket", hash_value, max_age=3600, secure=True, httponly=True)
+                if request.user.is_authenticated is False:
+                    response.set_cookie("basket", hash_value, max_age=3600, secure=True, httponly=True)
                 request.basket = basket_id
             return response
         else:
--- basket/models.py
+++ basket/models.py
@@ -8,10 +8,11 @@ from products.models import Products
 # Create your models here.
 
 class Basket(TimeStamp):
-    OPEN, SUBMITTED = ("Open", "Submitted")
+    OPEN, SUBMITTED, MERGED = ("Open", "Submitted", "Merged")
     STATUS = (
         (OPEN, "Open - currently active"),
         (SUBMITTED, "Submitted - has been ordered at the checkout"),
+        (MERGED, "Merged - has been merged with old basket"),
     )
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     status = models.CharField(choices=STATUS, default=OPEN, max_length=100)
@@ -33,6 +34,14 @@ class Basket(TimeStamp):
         basket  = Basket.objects.create()
         return basket
 
+    @classmethod
+    def get_basket(self, user):
+        basket = user.basket_set.last()
+        if basket.status == basket.OPEN:
+            return basket
+        else:
+            return self.create_basket(user)
+
     def create_basket_lines(self, product_id, quantity):
         product = Products.objects.get(id=product_id)
         if int(quantity) == 0:
@@ -44,6 +53,31 @@ class Basket(TimeStamp):
             line = BasketLine.objects.create(basket=self, product_id=product_id, quantity=int(quantity),
                                              price=product.price)
         return self.lines.count()
+
+    def merge_basket_lines(self, product_id, quantity):
+        product = Products.objects.get(id=product_id)
+        if int(quantity) == 0:
+            self.lines.filter(product_id=product_id).delete()
+        elif self.lines.filter(product_id=product_id).exists():
+            orginal_quantity = self.lines.filter(product_id=product_id).first().quantity
+            quantity += orginal_quantity
+            price = int(quantity) * product.price
+            self.lines.filter(product_id=product_id).update(quantity=int(quantity), price=price)
+        else:
+            line = BasketLine.objects.create(basket=self, product_id=product_id, quantity=int(quantity),
+                                             price=product.price)
+        return self.lines.count()
+
+    @classmethod
+    def merge_basket(cls, old_basket_id, new_basket_id):
+        if old_basket_id != new_basket_id:
+            old_basket = Basket.objects.get(id=old_basket_id)
+            new_basket = Basket.objects.get(id=new_basket_id)
+            if old_basket.status != Basket.MERGED:
+                for line in old_basket.lines.all():
+                    new_basket.merge_basket_lines(line.product_id, line.quantity)
+                old_basket.status = Basket.MERGED
+                old_basket.save()
 
     def get_product_line_count(self, product_id):
         if self.lines.filter(product_id=product_id).exists():
